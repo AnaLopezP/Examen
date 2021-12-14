832d9618da138501cdd2e2ac27600c531ad087b9
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-14 11:32:19 +0100

    funcion gana jugador uno

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,16 +2,46 @@ import random
 tablero = [[]]
 t = random.randint(1, 10) #numero de juegos a jugar
 n = random.randint(2, 2000) #tamaño del tablero
-r1 = random.randint(1, n)
-r2 = random.randint(1, n)
+r1 = random.randint(1, n) #fila de las torres del jugador 1
+r2 = random.randint(1, n) #fila de las torres del jugador 2
 
+#creacion del tablero
+def creacion_tablero(n):
+    i = n
+    j = n
+    tablero[i][j] = " "
+    for i in range(n):
+        for j in range(n):
+            tablero.append("a")
+    return tablero
+
+
+
+n = random.randint(2, 2000)
+i = n
+j = n
+creacion_tablero(n)
+
+def gana_jugador_uno():
+    if r1 == r2 - 1 and r2 == n:
+        return True
+    elif r2 == r1 - 1 and r1 == n:
+        return False
+    elif r1 == r2 + 1 and r2 == 0:
+        return True
+    elif r2 == r1 + 1 and r1 == 0:
+        return False
+    
 
 
 
 
 
 #codigo principal
+
+t = random.randint(1, 10)
 print(t)
+gana_jugador_uno()
 if gana_jugador_uno() == True:
     print("jugador 1")
 else:
