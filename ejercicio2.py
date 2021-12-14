3546661531b22dc8e4170042ebd10a10a1ad08a7
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-14 10:50:37 +0100

    datos del problema 2

---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+import random
+tablero = [[]]
+t = random.randint(1, 10) #numero de juegos a jugar
+n = random.randint(2, 2000) #tamaño del tablero
+r1 = random.randint(1, n)
+r2 = random.randint(1, n)
