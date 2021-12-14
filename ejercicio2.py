fce54681dc21fa7bbcac1d8f83eda63b94accc2f
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-14 12:11:31 +0100

    mejora codigo principal y funcion creacion tablero

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,23 +1,23 @@
 import random
-tablero = [[]]
 t = random.randint(1, 10) #numero de juegos a jugar
 n = random.randint(2, 2000) #tamaño del tablero
 r1 = random.randint(1, n) #fila de las torres del jugador 1
 r2 = random.randint(1, n) #fila de las torres del jugador 2
-
+tablero = []*n
 #creacion del tablero
 def creacion_tablero(n):
+    n
+    print(n)
     r1
     r2
-    i = n
-    j = n
-    print(n)
-    for i in range(len(tablero)):
-        for j in range(len(tablero)):
-            tablero[i] = tablero[r1]
-            tablero[r1][j] = "T1"
-            tablero[i] = tablero[r2] = "T2"
-            tablero[r2][j] = "T2"
+    print(r1)
+    print(r2)
+    for i in range(n, n):
+        for j in range(n, n):
+            if tablero[i] == r1:
+                tablero.append("T1")
+            elif tablero[i] == r2:
+                tablero.append("T2")
     return tablero
 
 # funcion gana jugador 1
@@ -31,19 +31,18 @@ def gana_jugador_uno():
     elif r2 == r1 + 1 and r1 == 0:
         return False
 
-
-
-
-
 #codigo principal
 n = random.randint(2, 2000)
 i = n
 j = n
 print(creacion_tablero(n))
-#t = random.randint(1, 10)
-#print(t)
-#gana_jugador_uno()
-#if gana_jugador_uno() == True:
- #   print("jugador 1")
-#else:
- #   print("jugador 2")
+t = random.randint(1, 10)
+print(t)
+gana_jugador_uno()
+while t !=0:
+    creacion_tablero(n)
+    if gana_jugador_uno() == True:
+        print("jugador 1")
+    else:
+        print("jugador 2")
+    t = t - 1
