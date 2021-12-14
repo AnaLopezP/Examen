1f819c147833abacd27a03df5849066a762d9554
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-14 11:48:57 +0100

    funcion creacion tablero ampliacion

--- ejercicio2.py
+++ ejercicio2.py
@@ -7,21 +7,20 @@ r2 = random.randint(1, n) #fila de las torres del jugador 2
 
 #creacion del tablero
 def creacion_tablero(n):
+    r1
+    r2
     i = n
     j = n
-    tablero[i][j] = " "
-    for i in range(n):
-        for j in range(n):
-            tablero.append("a")
+    print(n)
+    for i in range(len(tablero)):
+        for j in range(len(tablero)):
+            tablero[i] = tablero[r1]
+            tablero[r1][j] = "T1"
+            tablero[i] = tablero[r2] = "T2"
+            tablero[r2][j] = "T2"
     return tablero
 
-
-
-n = random.randint(2, 2000)
-i = n
-j = n
-creacion_tablero(n)
-
+# funcion gana jugador 1
 def gana_jugador_uno():
     if r1 == r2 - 1 and r2 == n:
         return True
@@ -31,18 +30,20 @@ def gana_jugador_uno():
         return True
     elif r2 == r1 + 1 and r1 == 0:
         return False
-    
 
 
 
 
 
 #codigo principal
-
-t = random.randint(1, 10)
-print(t)
-gana_jugador_uno()
-if gana_jugador_uno() == True:
-    print("jugador 1")
-else:
-    print("jugador 2")
+n = random.randint(2, 2000)
+i = n
+j = n
+print(creacion_tablero(n))
+#t = random.randint(1, 10)
+#print(t)
+#gana_jugador_uno()
+#if gana_jugador_uno() == True:
+ #   print("jugador 1")
+#else:
+ #   print("jugador 2")
