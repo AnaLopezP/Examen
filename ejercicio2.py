49627d765857c4d9443ff8de5b9aaa4ddb3f88c2
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-14 11:15:49 +0100

    codigo principal

--- ejercicio2.py
+++ ejercicio2.py
@@ -4,3 +4,15 @@ t = random.randint(1, 10) #numero de juegos a jugar
 n = random.randint(2, 2000) #tamaño del tablero
 r1 = random.randint(1, n)
 r2 = random.randint(1, n)
+
+
+
+
+
+
+#codigo principal
+print(t)
+if gana_jugador_uno() == True:
+    print("jugador 1")
+else:
+    print("jugador 2")
